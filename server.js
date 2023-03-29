0af1359c7b25a52f98172e0c0779fb63d0e4235a
Damin (Elena) Kim <elenanana0903@gmail.com>
2023-03-29 16:28:38 -0300

    add update and delete section

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 
     app.set('view engine','ejs')
     app.use(bodyParser.urlencoded({ extended: true}))
+    app.use(express.static('public'))
+    app.use(bodyParser.json())
+
+
     app.get('/', (req,res) => {
         
         quotesCollection.find().toArray()
@@ -31,6 +35,39 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
             })
             .catch(error => console.error(error))
     })
+    app.put('/quotes', (req,res) => {
+        quotesCollection.findOneAndUpdate(
+            { name: 'Yoda' },
+            {
+              $set: {
+                name: req.body.name,
+                quote: req.body.quote
+              }
+            },
+            {
+                upsert: true
+            }
+        )
+            .then(result => {
+                console.log(result)
+                res.json('Success')
+            })
+            .catch(error => console.error(error))
+          
+    })
+    app.delete('/quotes', (req, res) => {
+        quotesCollection.deleteOne(
+        { name: req.body.name }
+        )
+        .then(result => {
+            if (result.deletedCount === 0) {
+                return res.json('No quote to delete')
+              }
+            res.json(`Deleted Darth Vader's quote`)
+          })
+        .catch(error => console.error(error))
+    })
+
     app.listen(3000, function() {
         console.log('listening on 3000')
     })
